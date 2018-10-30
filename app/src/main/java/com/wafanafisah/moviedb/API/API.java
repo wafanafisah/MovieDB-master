fcf1be920be15f1bc228fe234136e411a3f519f9
wafanafisah <44542849+wafanafisah@users.noreply.github.com>
2018-10-30 09:23:00 +0700

    Initial commit

---
+++ app/src/main/java/com/wafanafisah/moviedb/API/API.java
@@ -0,0 +1,7 @@
+package com.wafanafisah.moviedb.API;
+
+public class API {
+    public static final String API_KEY = "3433e1d2e8383a1c3878aac19fb30e11";
+    public static final String URL_POPULAR = "https://api.themoviedb.org/3/movie/popular";
+    public static final String URL_GAMBAR = "http://image.tmdb.org/t/p/w300";
+}
